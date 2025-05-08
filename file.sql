ea78748ada44335cd4620c85ac32045799a75270
unknown <fabiolashami@gmail.com>
2025-05-08 00:15:10 -1200

    first comment

---
+++ file.sql
@@ -0,0 +1,4 @@
+INSERT INTO contacts VALUES (0, "fabiola","+250783456789","fabiola@gmail.com")
+
+
+//https://cdn.dribbble.com/users/6531036/avatars/normal/6ba4edc83c5a8be8373409a6ae4e8d8f.png?1705482123
